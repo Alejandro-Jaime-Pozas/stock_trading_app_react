0482151075ee1c4b4f71a5aaf0cf20dee2aef9ae
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2022-10-20 10:07:11 -0500

    btns added for stock tickers portfolio

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -10,9 +10,12 @@ import { apiKey, urlMain } from './Keys'
 
 export default function Portfolio(props) {
 
+    // THIS IS A TEST example
+    
+
     // console.log(props.loggedIn)
     let portfolio = ['AAPL', 'AMZN', 'ROKU']
-    const [quotes, setQuotes] = useState({})
+    const [quotes, setQuotes] = useState([])
     const [userStocks, setUserStocks] = useState([])
     // const [shares, setShares] = useState(null)
                 
@@ -39,7 +42,6 @@ export default function Portfolio(props) {
 
     // finnhub: need useEffect to fetch and store each stock price for each ticker in a new array, then display the desired data in return fn
     // okay, esta muy random lo que hace el useeffect con el fetch. osea agarra las stocks random sin orden y las imprime tres veces cada una.
-    // let prices = []
 
     // useEffect(() => {
     //     // try creating a list here, add info to list, then set the state to that list after for loop...
@@ -60,7 +62,7 @@ export default function Portfolio(props) {
     // let xlist = []
     // xlist.push('something here')
     // console.log(xlist[0])
-        
+
     const handleClick = e => {
         // console.log(e.target.innerText)
         props.changeTicker(e.target.innerText)
@@ -78,33 +80,32 @@ export default function Portfolio(props) {
     return (
         <div>
             <div className='row'>
-                <h1>{quotes.c}</h1>
+                {/* <h1>{quotes.c}</h1> */}
                 <h1>My Portfolio</h1>
                 <p className='lead'>${portfolioValue()}</p>
             </div>
+            {/* graph goes here */}
             <div className="row border rounded m-3 " style={{height: '15vh'}}></div>
             <div className='row '>
-                <div className="row">
-                    <p className="col">Ticker</p>
-                    <p className="col">Current Price</p>
-                    <p className="col">My Shares</p>
-                    <p className="col">My Total Value</p>
+                <div className="row table_headers">
+                    <p className="col-3">Ticker</p>
+                    <p className="col-3">Current Price</p>
+                    <p className="col-3">My Shares</p>
+                    <p className="col-3">My Total Value</p>
                 </div>
 
                 {/* this will be the data output for userStocks */}
                 {/* the reason this doesn't update is it's not linked at all to the stock's real price. need to do multiple fetches one for each stock user owns to get real time quote */}
-                <div className="">
                     {userStocks.map((stock, i) => {
                         return (
                             <div key={i} className="row border-top align-items-center">
-                                <Link to='/stock' onClick={handleClick} className="col">{stock.ticker}</Link>
-                                <p className="col mt-3">${stock.new_price?.toFixed(2).toLocaleString()}</p>
-                                <p className="col mt-3">{stock.total_shares?.toLocaleString()}</p>
-                                <p className="col mt-3">${(stock.real_value?.toFixed(2)).toLocaleString()}</p>
+                                <Link to='/stock' onClick={handleClick} className="col-3 btn btn-dark ">{stock.ticker}</Link>
+                                <p className="col-3 mt-3 ">${stock.new_price?.toFixed(2).toLocaleString()}</p>
+                                <p className="col-3 mt-3 ">{stock.total_shares?.toLocaleString()}</p>
+                                <p className="col-3 mt-3 ">${(stock.real_value?.toFixed(2)).toLocaleString()}</p>
                             </div>
                         )
                     })}
-                </div>
             </div>
             {/* <div className="row my-5">
                 {userStocks[0] ? null : <Link to='/search' className='btn btn-dark w-50 align-self-center col' >Start Searching for some stocks</Link>}
