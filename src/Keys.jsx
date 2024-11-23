8e05a12decd7a3e7ac303c7f4e0e150fbc88334f
Alejandro-Jaime-Pozas <ajaimepozas@gmail.com>
2024-11-23 13:43:16 -0600

    changed js files to jsx files

---
+++ src/Keys.jsx
@@ -0,0 +1,9 @@
+export const apiKey = "cc4f982ad3ia9srm3fog" // api key for finnhub
+export const apiKeyAlpha = "9CE9HIZZK3YDO97Y" // api key for alphavantage
+export const urlMain = "https://stock-app-flask-v3.glitch.me" // this will fetch from public glitch app
+// export const urlMain = "http://127.0.0.1:5000" // this will fetch from local flask app
+
+// create way to move back and forth automatically bw web server vs local server
+// export const urlMain2 = async () => {
+    // return server running === 'http://127.0.0.1:5000' ? 'http://127.0.0.1:5000' : 'heroku/firebase/glitch URL'
+// }
